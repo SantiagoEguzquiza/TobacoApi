d60fcd871315ce80d9f5c02e28d5e2832db7ac19
Rodrigo Denis <114033291+RodriDS12@users.noreply.github.com>
2025-10-15 12:17:29 -0300

    Fix EstadoEntrega

--- TobacoBackend/TobacoBackend/obj/Debug/net8.0/TobacoBackend.AssemblyInfo.cs
+++ TobacoBackend/TobacoBackend/obj/Debug/net8.0/TobacoBackend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TobacoBackend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c7eb9087d6f4bb2d7b5015f1a9ab61f07cdcd54e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a7e8724e79073e874e86cd08b70b4867bb2704ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("TobacoBackend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TobacoBackend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
